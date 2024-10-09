1d42d37a1900acfa630ad8cecabff4407748d665
Hamdiibrahim <yusufhamdi137@gmail.com>
2024-10-09 09:04:50 +0300

    simplerliker

--- main.js
+++ main.js
@@ -2,10 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const glyphStates = {
+  [EMPTY_HEART]: FULL_HEART,
+  [FULL_HEART]: EMPTY_HEART
+}
+//define state for heart colors
+const colorStates = {
+  "": "red", 
+  "red": ""   
+};
+
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+function showerrorModal(errorMessage){
+  const errorModal = document.getElementById('errorModal')
+  const errorMessageElement = document.getElementById('errorMessage')
+  errorMessageElement.textContent = errorMessage
+  errorModal.classList.remove('hidden')
+}
 
+setTimeout(() => {
+  errorModal.classList.add('hidden');
+}, 3000);
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      heart.innerText = FULL_HEART;
+      heart.classList.add('activated-heart')
+    })
+    .catch(function(error) {
+      showerrorModal(error);
+    });
+}
+//add click event listener to each heart
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
